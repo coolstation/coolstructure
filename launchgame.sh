9c1ba2023e0d7898b4856630eaadc9a17c87f3ef
Tamber <tamber@furryhelix.co.uk>
2022-07-20 21:56:29 +0100

    The hardest of reboots

--- launchgame.sh
+++ launchgame.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+LIVEDIR=/home/ss13/coolserv/live
+
 for t in {1..12}
 do
     if [ -z "$(ps -C DreamDaemon | grep DreamDaemon)" ]
     then
-        /home/ss13/byond/bin/DreamDaemon /home/ss13/coolserv/live/coolstation.dmb 8085 -trusted &
+	# If this file exists, then DreamDaemon shuts down on world/Reboot(). It then requires
+	# something external to start it back up again. Oh, hey, it us; the external something!
+	touch ${LIVEDIR}/data/hard-reboot
+        /home/ss13/byond/bin/DreamDaemon "${LIVEDIR}/coolstation.dmb" 8085 -trusted &
     fi
     sleep 5
 done
