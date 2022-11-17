78044f91ac5e99773b65ad86143fbe2b134cf076
warc <tin.warc@gmail.com>
2022-11-16 22:10:24 -0500

    websight?

---
+++ updatewebsite.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CODEBASE_DIR="/home/ss13/webfucker/git-repo"
+
+# Grab updated source
+cd ${CODEBASE_DIR}
+git pull --recurse-submodules
+
+if [ $? -ne 0 ]; then
+   # Either the git pull, or the git merge, failed. *sad-trombone*
+   # Sadly, that's all we know. :(
+   # There's no documentation for what other error codes there are, or what they mean.
+   exit 1
+fi
+
+# Fire the assets down the tube to the cdn
+rsync -avz --delete --exclude ".htaccess" --exclude ".ftpquota" \
+      ${CODEBASE_DIR} coolstation.space:~/www/
+
+if [ $? -ne 0 ]; then
+    # rsync went wrong :(
+    exit 2
+fi
+
