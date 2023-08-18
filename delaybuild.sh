00ca29754bda115f1440b95341b2b0556ded7be3
Tamber <tamber@furryhelix.co.uk>
2023-08-18 18:28:44 +0100

    scrunge the check a bit. also add some debug output

--- delaybuild.sh
+++ delaybuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPTHOLE=/home/ss13/coolstructure
+
 if [ -z $1 ]
 then
     exit -1
@@ -8,10 +10,12 @@ fi
 sleep 5m
 
 # Check the game server is still alive.
-if [[ "$(ps -q $1 -o comm=)" == "DreamDaemon" ]]
+if [[ -n "$(ps -q $1 -o comm=)" ]]
 then
     # Server's still up, I guess! Launch the build script!
-    ./buildfusilli.sh
+    $(SCRIPTHOLE)/buildfusilli.sh
+else
+    echo "`date --iso-8601=seconds` - Couldn't find DreamDaemon at $1: '$(ps -q $1 -o comm=)'." > $(SCRIPTHOLE)/delay_fail
 fi
 
 # Our work here is done.
